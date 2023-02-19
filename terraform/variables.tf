e623c0b9589fdb1d3991a6dfb97bbc7db3552f59
chiella <bemnjichiella@yahoo.com>
2023-02-19 09:45:16 +0100

    adjusting ip

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,11 +11,11 @@ variable "availability_zone_id" {
     default = "ap-south-1a"
 }
 variable "my_ip" {
-    default = "154.72.167.101/32"
+    default = "154.72.150.66/32"
 }
 variable "instance_type" {
     default = "t2.micro"
 }
 variable "jenkins_ip" {
-    default = "43.205.115.91/32"
+    default = "3.110.215.49/32"
 }
