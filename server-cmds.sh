e33a4ab37f36e1e6796da46b589d6de2bf49a8c0
chiella <bemnjichiella@yahoo.com>
2023-02-19 10:55:25 +0100

    deploying app

--- server-cmds.sh
+++ server-cmds.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export $IMAGE=$1
-export $DOCKER_USER=$2
-export $DOCKER_PWD=$3
+export IMAGE=$1
+export DOCKER_USER=$2
+export DOCKER_PWD=$3
 
 echo $DOCKER_PWD | docker login -u $DOCKER_USER --password-stdin
 docker-compose -f docker-compose.yaml up --detach
